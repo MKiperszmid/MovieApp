4791bc7fc6856bef3bb0c357b2b43a7234937eae
Martin Kiperszmid <mkiperszmid@gmail.com>
2021-03-05 21:47:02 -0300

    Se agregan las dependencias basicas para el proyecto

---
+++ app/src/main/java/com/rs/movieapp/view/ListFragment.kt
@@ -0,0 +1,17 @@
+package com.rs.movieapp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.rs.movieapp.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+}
