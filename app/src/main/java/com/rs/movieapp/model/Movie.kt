847055062aeb35547d931dec5a779c0b30268e60
Martin Kiperszmid <mkiperszmid@gmail.com>
2021-03-07 19:13:21 -0300

    Cambios menores de sobre como un fragment se agrega

--- app/src/main/java/com/rs/movieapp/model/Movie.kt
+++ app/src/main/java/com/rs/movieapp/model/Movie.kt
@@ -3,6 +3,7 @@ package com.rs.movieapp.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import java.text.SimpleDateFormat
 import java.util.*
 
 @Entity
@@ -16,11 +17,11 @@ data class Movie(
     @SerializedName("original_name")
     private val _title: String? = "",
     @SerializedName("overview")
-    private val _overview: String = "",
+    private val _overview: String? = "",
     @SerializedName("poster_path")
-    private val _posterImage: String = "",
+    private val _posterImage: String? = "",
     @SerializedName("first_air_date")
-    private val _releaseDate: Date = Date(),
+    private val _releaseDate: String? = "",
     var saved: Boolean = false
 ) {
     val backgroundImage
@@ -39,12 +40,17 @@ data class Movie(
         get() = _posterImage ?: ""
 
     val releaseDate
-        get() = _releaseDate ?: Date()
+        get() = _releaseDate ?: ""
 
 
     fun getYear(): Int {
         val calendar = Calendar.getInstance()
-        calendar.time = releaseDate
+        if (releaseDate == "") {
+            calendar.time = Date()
+        } else {
+            calendar.time = SimpleDateFormat("yyyy-MM-dd").parse(releaseDate)
+        }
+
         return calendar.get(Calendar.YEAR)
     }
 }
